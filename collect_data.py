d2a8b218e93018d1a5ad35caa090100b67169efb
Kaito <mail.kaito03@gmail.com>
2020-05-19 03:49:58 +0900

    [update] スクレイピングスクリプトの完成

--- collect_data.py
+++ collect_data.py
@@ -1,24 +1,55 @@
 from selenium.webdriver.common.keys import Keys
-from typing import Dict, Any
+import requests
+import time
+import os
+from typing import List, Optional
 
 from scrape.chrome_handler import ChromeHandler
-from scrape.settings import XPATHS, BASE_URL
+from scrape.settings import XPATHS, BASE_URL, BASE_DIR, TIME_SPAN
+
+
+class Result:
+    def __init__(self, age: int, _id: int, link: str) -> None:
+        self.age = age
+        self._id = _id
+        self.link = link
+        self.path: Optional[str] = None
+
+    def __str__(self) -> str:
+        return "{}. {} => {}".format(self._id, self.age, self.link)
+
+    __repr__ = __str__
+
+    def save(self) -> None:
+        res = requests.get(BASE_URL + self.link)
+        extension = None
+
+        if res.headers['content-type'] == 'image/jpeg':
+            extension = 'jpg'
+        elif res.headers['content-type'] == 'image/png':
+            extension = 'png'
+        else:
+            # 未対応形式
+            return None
+
+        filename = f"{self.age}_{self._id:0=4}.{extension}"
+        self.path = os.path.join(BASE_DIR, 'data', filename)
+
+        with open(self.path, "wb") as f:
+            f.write(res.content)
 
 
 class Main:
     def __init__(self, browser: bool = False, init_id: int = 0) -> None:
+        """
+        Args:
+            browser(bool):  Seleniumでブラウザを開くかどうか
+            init_id(int): 初期画像ID(既存のMAX IDより大きい値を渡す)
+        """
         self.handler = ChromeHandler(browser=browser)
 
-        self.id = init_id
-        self.results: Dict[str, Any] = {}
-        """
-        {
-            "age_id": {
-                "age": 28,
-                "image_path": "/path/to/hoge/age_id.png",
-            }
-        }
-        """
+        self._id = init_id
+        self.results: List[Result] = []
 
         # 1. 初期ページへアクセス
         self.handler.access(BASE_URL, cl='firstChild')
@@ -28,31 +59,51 @@ class Main:
             XPATHS['search_button']
         ).send_keys(Keys.ENTER)
         self.handler.wait(_id="search-results")
+        self.wait()
 
-    def read_table(self) -> None:
-        table_lines = self.handler.driver.find_element_by_id("search-results") \
-            .find_element_by_tag_name('tbody') \
-            .find_elements_by_tag_name('tr')
+    @staticmethod
+    def wait() -> None:
+        time.sleep(TIME_SPAN)
 
-        for line in table_lines:
-            # tbodyにヘッダもなぜか含まれてるので, スキップする
-            if line.get_attribute('class') != 'firstChild':
-                # メインロジック
-                link = line.find_element_by_class_name('firstChild') \
-                    .find_element_by_tag_name('a') \
-                    .get_attribute('href')
-                age = line.find_element_by_class_name('lastChild')
-                # .get_attribute("textContent")
+    def move_to_next(self):
+        self.handler.driver.find_element_by_xpath(
+            XPATHS['next_button']
+        ).send_keys(Keys.ENTER)
+        self.handler.wait(_id="search-results")
+        self.wait()
 
-                # print(link, ' => ', age, '...')
-                print('age', age)
-                print('age.text', age.text)
-                print('age.get_attrivute', age.get_attribute('textContent'))
-                print('attrs', age.__dict__)
+    def read_table(self, recursive=False) -> None:
+        self.handler.set_soup()
+
+        for line in self.handler.soup.find("table", {"id": "search-results"}).findAll("tr"):
+            image_td = line.find("td", {"class": "firstChild"})
+            if image_td is None:
+                # 最初の一行は th なので, みつからない => continue
+                continue
+
+            link = image_td.find("a").find('img').get('src')
+            age = line.find("td", {"class": "lastChild"}).get_text()
+
+            if age.isdigit():
+                result = Result(age=int(age), _id=self._id, link=link)
+                result.save()
+                self.results.append(result)
+                self._id = self._id + 1
+                self.wait()
+
+                print(result)  # Log
+
+        if recursive:
+            self.wait()
+
+            # 次のページへ行き, 再び同様の処理
+            self.move_to_next()
+            self.read_table(recursive=True)
 
     def run(self) -> None:
-        self.read_table()
+        self.read_table(recursive=True)
+        self.handler.fin()
 
 
 if __name__ == '__main__':
-    Main(browser=True).run()
+    Main(browser=False, init_id=105).run()
--- scrape/settings.py
+++ scrape/settings.py
@@ -1,7 +1,7 @@
 import os
 
 BASE_DIR = os.getcwd()
-BASE_URL: str = "https://www.talent-databank.co.jp/"
+BASE_URL: str = "https://www.talent-databank.co.jp"
 CHROME_DRIVER_PATH: str = os.path.join(
     os.path.expanduser('~'), 'Selenium', 'chromedriver'
 )
@@ -11,3 +11,5 @@ XPATHS = {
     "table_path": "/html/body/div[1]/div/div[3]/div[2]/div[2]/div/div[2]/table/tbody",
     "next_button": "/html/body/div[1]/div/div[3]/div[2]/div[2]/div/div[1]/span/a[5]"
 }
+
+TIME_SPAN = 10  # アクセス秒間隔, 対象サイト負荷軽減のため一定より大きくすること
