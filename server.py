ddf70204a97300ad1d4f87826163ccad61604074
Kaito <mail.kaito03@gmail.com>
2020-05-18 19:16:41 +0900

    [update] backendの実装

--- server.py
+++ server.py
@@ -1,25 +1,68 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, make_response
+import uuid
 import os
 
-from web_app.settings import DEBUG, HOST, PORT, BASE_DIR
+from web_app.settings import DEBUG, HOST, PORT, BASE_DIR, PRODUCT_URL
 
 app = Flask(
     __name__,
     template_folder=os.path.join(BASE_DIR, 'web_app', 'templates'),
     static_folder=os.path.join(BASE_DIR, 'web_app', 'static'),
 )
+# ⇓ 本番環境では, 環境変数から取るようにする ⇓
+app.secret_key = 'jskdjkfsakfjkldsajfdskafjdsk;a'
+
+
+def get_base_context():
+    return {
+        'success_message': session.pop('success_message', None),
+        'error_message': session.pop('error_message', None),
+    }
 
 
 @app.route('/')
 def index():
-    context = {}
-    return render_template('index.html', context=context)
+    context = get_base_context()
+    return render_template('index.html', **context)
+
+
+@app.route('/estimate/', methods=['POST'])
+def estimate():
+    file = request.files.get('face_image')
+
+    if file.filename == '':
+        # ファイルが送られてきていないので, TOPへリダイレクトする
+        session['error_message'] = 'ファイルをアップロードしてください'
+        return redirect(url_for('index'))
+    else:
+        # ファイルを保存
+        file = request.files.get('face_image')
+
+        # 一意なファイル名 + 拡張子でOSに保存する
+        filename = str(uuid.uuid4()) + '.' + file.filename.split('.')[-1]
+        file.save(os.path.join(BASE_DIR, 'web_app', 'static', 'image', filename))
+
+        # 保存したファイルから機械学習モデルにかける
+        # ロジックは, 未実装なので仮としておく
+
+        # 仮の結果
+        session['estimated_age'] = 23
+        session['result_image_path'] = '/static/image/' + filename
+
+        return redirect(url_for('result'))
 
 
 @app.route('/result/')
 def result():
-    context = {}
-    return render_template('result.html', context=context)
+    context = get_base_context()
+
+    context.update({
+        "product_url": PRODUCT_URL,
+        "estimated_age": session['estimated_age'],
+        "result_image_path": session['result_image_path']
+    })
+
+    return render_template('result.html', **context)
 
 
 if __name__ == '__main__':
--- web_app/settings.py
+++ web_app/settings.py
@@ -5,3 +5,7 @@ BASE_DIR = os.getcwd()
 DEBUG = True
 HOST = 'localhost'
 PORT = 8080
+
+PRODUCT_URL = 'https://xxx.com'  # まだ本番環境の想定がないので, とりあえず
+if DEBUG:
+    PRODUCT_URL = 'http://localhost:8080'
