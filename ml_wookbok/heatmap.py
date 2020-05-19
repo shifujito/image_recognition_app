ab5f1d5fb2f134c9649f68f6c2f80233cf80a213
Kaito <mail.kaito03@gmail.com>
2020-05-19 13:41:00 +0900

    [update] ヒートマップの出力に対応

--- ml_wookbok/heatmap.py
+++ ml_wookbok/heatmap.py
@@ -4,14 +4,14 @@ from keras import backend as K
 
 
 def make_hetamap(model, X, path):
-    img_output = model.output[:,1]
+    img_output = model.output[:, 1]
     last_conv_layer = model.get_layer('conv2d_1')
     grads = K.gradients(img_output, last_conv_layer.output)[0]
-    pooled_grads = K.mean(grads, axis=(0,1,2))
-    iterate = K.function([model.input],[pooled_grads, last_conv_layer.output[0]])
+    pooled_grads = K.mean(grads, axis=(0, 1, 2))
+    iterate = K.function([model.input], [pooled_grads, last_conv_layer.output[0]])
     pooled_grads_value, conv_layer_output_value = iterate([X])
     for i in range(512):
-        conv_layer_output_value[: , : , i] *= pooled_grads_value[i]
+        conv_layer_output_value[:, :, i] *= pooled_grads_value[i]
     heatmap = np.mean(conv_layer_output_value, axis=-1)
     heatmap = np.maximum(heatmap, 0)
     heatmap /= np.max(heatmap)
--- ml_wookbok/main.py
+++ ml_wookbok/main.py
@@ -7,14 +7,23 @@ from keras import backend as K
 
 
 def main(test_path):
+    """ 入力画像のパスから年齢推定をする関数
+    ヒートマップを同じパスに上書きする
+
+    Args:
+        test_path: 入力画像のパス
+
+    Returns(str): 予測年齢
+
+    """
     img = pp.load_image(test_path)
     model = model_from_json(open('./ml_wookbok/vgg16_model.json').read())
     model.load_weights('./ml_wookbok/vgg16_model.hdf5')
     pred = np.argmax(model.predict(np.array([img])), axis=1)
     pred_age = al.add_name(pred)
-    out_path = ht.make_hetamap(model, np.array([img]),test_path)
+    out_path = ht.make_hetamap(model, np.array([img]), test_path)
     K.clear_session()
-    return pred_age, out_path 
+    return pred_age  # , out_path , 上書きなので戻り値にしない
 
 
 if __name__ == '__main__':
